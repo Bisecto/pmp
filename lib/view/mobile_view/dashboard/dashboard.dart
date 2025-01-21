4d7c3e03c08d856d30fd22f1a26344c3eee54d07
precious <bisector0@gmail.com>
2025-01-21 22:22:56 +0100

    added refresh indicator

--- lib/view/mobile_view/dashboard/dashboard.dart
+++ lib/view/mobile_view/dashboard/dashboard.dart
@@ -27,7 +27,6 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-
   int semesterId = 0;
   bool isImageNull = false;
   bool isImageAccessible = true;
@@ -48,192 +47,201 @@ class _DashboardState extends State<Dashboard> {
     return Scaffold(
       backgroundColor:
           theme.isDark ? AppColors.darkBackgroundColor : AppColors.white,
-      body: SafeArea(
-          child: SingleChildScrollView(
-        physics: const ScrollPhysics(),
-        child: Column(
-          children: [
-            CustomAppBar(userModel: widget.userModel),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SvgPicture.asset(
-                  AppSvgImages.finger,
-                  height: 20,
-                  width: 20,
-                ),
-                const SizedBox(
-                  width: 5,
-                ),
-                TextStyles.textSubHeadings(
-                    textValue: 'Welcome ${widget.userModel.firstName}!',
-                    textSize: 20),
-              ],
-            ),
-            BlocConsumer<PropertyBloc, PropertyState>(
-                bloc: propertyBloc,
-                listenWhen: (previous, current) => current is! PropertyInitial,
-                buildWhen: (previous, current) => current is! PropertyInitial,
-                listener: (context, state) {
-                  if (state is PropertySuccessState) {
-                    //MSG.snackBar(context, state.msg);
+      body: RefreshIndicator(
+        onRefresh: _pullRefresh,
+        child: SafeArea(
+            child: SingleChildScrollView(
+          physics: const ScrollPhysics(),
+          child: Column(
+            children: [
+              CustomAppBar(userModel: widget.userModel),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset(
+                    AppSvgImages.finger,
+                    height: 20,
+                    width: 20,
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  TextStyles.textSubHeadings(
+                      textValue: 'Welcome ${widget.userModel.firstName}!',
+                      textSize: 20),
+                ],
+              ),
+              BlocConsumer<PropertyBloc, PropertyState>(
+                  bloc: propertyBloc,
+                  listenWhen: (previous, current) =>
+                      current is! PropertyInitial,
+                  buildWhen: (previous, current) => current is! PropertyInitial,
+                  listener: (context, state) {
+                    if (state is PropertySuccessState) {
+                      //MSG.snackBar(context, state.msg);
 
-                    // AppNavigator.pushAndRemovePreviousPages(context,
-                    //     page: LandingPage(studentProfile: state.studentProfile));
-                  } else if (state is PropertyErrorState) {
-                    MSG.warningSnackBar(context, state.error);
-                  }
-                },
-                builder: (context, state) {
-                  switch (state.runtimeType) {
-                    // case PostsFetchingState:
-                    //   return const Center(
-                    //     child: CircularProgressIndicator(),
-                    //   );
-                    case PropertySuccessState:
-                      final propertySuccessState =
-                          state as PropertySuccessState;
+                      // AppNavigator.pushAndRemovePreviousPages(context,
+                      //     page: LandingPage(studentProfile: state.studentProfile));
+                    } else if (state is PropertyErrorState) {
+                      MSG.warningSnackBar(context, state.error);
+                    }
+                  },
+                  builder: (context, state) {
+                    switch (state.runtimeType) {
+                      // case PostsFetchingState:
+                      //   return const Center(
+                      //     child: CircularProgressIndicator(),
+                      //   );
+                      case PropertySuccessState:
+                        final propertySuccessState =
+                            state as PropertySuccessState;
 
-                      return Padding(
-                        padding: const EdgeInsets.all(10.0),
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Container(
-                                padding: const EdgeInsets.all(10.0),
-                                decoration: BoxDecoration(
-                                  border:
-                                      Border.all(color: AppColors.mainAppColor),
-                                  borderRadius: BorderRadius.circular(8.0),
-                                ),
-                                child: Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  children: [
-                                    // Properties
-                                    RichText(
-                                      text: TextSpan(
-                                        children: [
-                                          const TextSpan(
-                                            text: 'Properties: ',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: 13.0,
+                        return Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Container(
+                                  padding: const EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: AppColors.mainAppColor),
+                                    borderRadius: BorderRadius.circular(8.0),
+                                  ),
+                                  child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      // Properties
+                                      RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            const TextSpan(
+                                              text: 'Properties: ',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black,
+                                                fontSize: 13.0,
+                                              ),
                                             ),
-                                          ),
-                                          TextSpan(
-                                            text:
-                                                '${propertySuccessState.propertiesModel.totalProperties}  ',
-                                            style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 13.0,
+                                            TextSpan(
+                                              text:
+                                                  '${propertySuccessState.propertiesModel.totalProperties}  ',
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 13.0,
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    // Rooms
-                                    RichText(
-                                      text: TextSpan(
-                                        children: [
-                                          const TextSpan(
-                                            text: 'Total Space: ',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: 13.0,
+                                      // Rooms
+                                      RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            const TextSpan(
+                                              text: 'Total Space: ',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black,
+                                                fontSize: 13.0,
+                                              ),
                                             ),
-                                          ),
-                                          TextSpan(
-                                            text:
-                                                '${propertySuccessState.propertiesModel.totalSpaces}  ',
-                                            style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 13.0,
+                                            TextSpan(
+                                              text:
+                                                  '${propertySuccessState.propertiesModel.totalSpaces}  ',
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 13.0,
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                    // RichText(
-                                    //   text: TextSpan(
-                                    //     children: [
-                                    //       const TextSpan(
-                                    //         text: 'Occupied Space: ',
-                                    //         style: TextStyle(
-                                    //           fontWeight: FontWeight.bold,
-                                    //           color: Colors.black,
-                                    //           fontSize: 13.0,
-                                    //         ),
-                                    //       ),
-                                    //       TextSpan(
-                                    //         text:
-                                    //             '${propertySuccessState.propertiesModel.totalOccupiedRooms}  ',
-                                    //         style: const TextStyle(
-                                    //           color: Colors.black,
-                                    //           fontSize: 13.0,
-                                    //         ),
-                                    //       ),
-                                    //     ],
-                                    //   ),
-                                    // ),
-                                    // Tenants
-                                    RichText(
-                                      text: TextSpan(
-                                        children: [
-                                          const TextSpan(
-                                            text: 'Occupied Space: ',
-                                            style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              color: Colors.black,
-                                              fontSize: 13.0,
+                                      // RichText(
+                                      //   text: TextSpan(
+                                      //     children: [
+                                      //       const TextSpan(
+                                      //         text: 'Occupied Space: ',
+                                      //         style: TextStyle(
+                                      //           fontWeight: FontWeight.bold,
+                                      //           color: Colors.black,
+                                      //           fontSize: 13.0,
+                                      //         ),
+                                      //       ),
+                                      //       TextSpan(
+                                      //         text:
+                                      //             '${propertySuccessState.propertiesModel.totalOccupiedRooms}  ',
+                                      //         style: const TextStyle(
+                                      //           color: Colors.black,
+                                      //           fontSize: 13.0,
+                                      //         ),
+                                      //       ),
+                                      //     ],
+                                      //   ),
+                                      // ),
+                                      // Tenants
+                                      RichText(
+                                        text: TextSpan(
+                                          children: [
+                                            const TextSpan(
+                                              text: 'Occupied Space: ',
+                                              style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                color: Colors.black,
+                                                fontSize: 13.0,
+                                              ),
                                             ),
-                                          ),
-                                          TextSpan(
-                                            text:
-                                                '${propertySuccessState.propertiesModel.totalTenants}',
-                                            style: const TextStyle(
-                                              color: Colors.black,
-                                              fontSize: 13.0,
+                                            TextSpan(
+                                              text:
+                                                  '${propertySuccessState.propertiesModel.totalTenants}',
+                                              style: const TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 13.0,
+                                              ),
                                             ),
-                                          ),
-                                        ],
+                                          ],
+                                        ),
                                       ),
-                                    ),
-                                  ],
+                                    ],
+                                  ),
                                 ),
                               ),
-                            ),
-                            LodgeList(
-                              properties: propertySuccessState
-                                  .propertiesModel.properties,
-                              userModel: widget.userModel,
-                            ),
-                          ],
-                        ),
-                      );
+                              LodgeList(
+                                properties: propertySuccessState
+                                    .propertiesModel.properties,
+                                userModel: widget.userModel,
+                              ),
+                            ],
+                          ),
+                        );
 
-                    case PropertyLoadingState:
-                      return const Column(
-                        children: [
-                          AppLoadingPage("Fetching Properties..."),
-                        ],
-                      );
-                    default:
-                      return const Column(
-                        children: [
-                          AppLoadingPage("Fetching Properties..."),
-                        ],
-                      );
-                  }
-                })
-          ],
-        ),
-      )),
+                      case PropertyLoadingState:
+                        return const Column(
+                          children: [
+                            AppLoadingPage("Fetching Properties..."),
+                          ],
+                        );
+                      default:
+                        return const Column(
+                          children: [
+                            AppLoadingPage("Fetching Properties..."),
+                          ],
+                        );
+                    }
+                  })
+            ],
+          ),
+        )),
+      ),
     );
+  }
+
+  Future<void> _pullRefresh() async {
+    propertyBloc.add(GetPropertyEvent());
   }
 
   Set active = {};
